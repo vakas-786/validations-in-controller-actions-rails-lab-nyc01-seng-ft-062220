849da2ff6c45d8aa25a4f92ca22552f728dd3736
vakas-786 <vakas0959@gmail.com>
2020-07-22 23:47:08 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+    validates(:name, { :length => { :minimum => 2 } })
+    validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+        message: "%{value} is not a valid category" }
 end
